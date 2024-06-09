b3602b8d0672c81b9ea1468037bcc2c9fad3720d
TungLamC <841213323@qq.com>
2024-06-09 06:13:44 +0000

    deploy: https://github.com/TungLamC/Roam/commit/594960cddbf03b910c37433d55698f2040cf7a86

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["C_23_中的_GCHandle/","Cinemachine_源码解析/","Hexo_Anzhiyu_主题配置_giscus_评论系统/","Knuth_Shuffle_洗牌算法/","Logseq_hierarchy/","Logseq_查询不在特定命名空间下的页面/","Markdown_实现_Callout/","SVN_提交所有修改/","关于/","多个_Unity_进程使用一个_Unity_工程/","跳笔表达/"];function toRandomPost(){
+var posts=["C_23_中的_GCHandle/","Cinemachine_源码解析/","Knuth_Shuffle_洗牌算法/","Logseq_hierarchy/","Logseq_查询不在特定命名空间下的页面/","Markdown_实现_Callout/","SVN_提交所有修改/","hexo-theme-anzhiyu_主题配置_giscus_评论系统/","关于/","多个_Unity_进程使用一个_Unity_工程/","跳笔表达/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
